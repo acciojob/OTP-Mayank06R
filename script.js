fda7845be81517f3d888d3631da2fd79339d7141
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-19 09:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, idx) => {
+    code.addEventListener('input', (e) => {
+        if (e.target.value.length === 1) {
+            if (idx < codes.length - 1) {
+                codes[idx + 1].focus();
+            }
+        }
+    });
+
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && e.target.value.length === 0 && idx > 0) {
+            codes[idx - 1].focus();
+        }
+    });
+});
